59ed2a7a8216c537bc7b4f1b3f8d9c98297edf88
AvedisDem <avedis.demirjian901@gmail.com>
2023-05-08 18:21:32 +0400

    red

--- src/Skills.js
+++ src/Skills.js
@@ -7,6 +7,7 @@ const testData = [
   { bgcolor: "#00695c", completed: 45 , title: "CSS"},
   { bgcolor: "#ef6c00", completed: 45 , title: "HTML"},
   { bgcolor: "#447799", completed: 47 , title: "JS"},
+  { bgcolor: "#128c24", completed: 45 , title: "Redux"},
   { bgcolor: "#123499", completed: 44 , title: "C#"},
   { bgcolor: "#993255", completed: 40 , title: "Unity"},
   { bgcolor: "#5bca50", completed: 38 , title: "C++"},
