98917be6592598c6d80911f1471a40aca66f9f10
AvedisDem <avedis.demirjian901@gmail.com>
2023-05-28 10:35:33 +0400

    fix widget error

--- src/PcWeb/WidgetsP.js
+++ src/PcWeb/WidgetsP.js
@@ -3,7 +3,7 @@ import './Styles/WidgetsP.css';
 
 function WidgetsP({img,desc}) {
   return (
-    <div className='widgetsp' >
+    <div className='widgetsp' style={{position:'relative'}} >
         <img src={img}/>
         <p>{desc}</p>
     </div>
