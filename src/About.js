881e6924b2f7a4ad8f9ed9edb2ddd7c46b31a9b7
AvedisDem <117506784+avedisdem@users.noreply.github.com>
2024-06-12 11:11:21 -0700

    Update About.js
    Changing some words

--- src/About.js
+++ src/About.js
@@ -13,9 +13,9 @@ function About() {
         <div className="about__leftPart">
           <h1>Awadis Demirjian</h1>
             <br/>
-            <span style = {{fontWeight:"900"}}>A Front-End Developer</span>
+            <span style = {{fontWeight:"900"}}>Interseted In Network Engineering</span>
+            <span>Planning to take the <spanstyle = {{fontWeight:"900"}}> CCNA </span>certificate</span>
             <span>Knowledge in <span style = {{fontWeight:"900"}} > Javascript</span> including:</span> 
-            
             <span style = {{fontWeight:"900"}}>React, Redux</span>
             <span>Competent in <span style = {{fontWeight:"900"}}> C#</span> and <span style = {{fontWeight:"900"}}> Unity</span> </span>
         </div>
