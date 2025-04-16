79c82ab56e2e20a1fadadb6b2305cff16c200c94
AvedisDem <117506784+avedisdem@users.noreply.github.com>
2025-04-16 08:39:38 -0700

    Update Skills.js

--- src/Skills.js
+++ src/Skills.js
@@ -3,15 +3,15 @@ import './Style/Skills.css'
 import ProgressBar from "./ProgressBar";
 
 const testData = [
-  { bgcolor: "#6a1b9a", completed: 50 , title: "React"},
-  { bgcolor: "#231cca", completed: 35 , title: "NextJS"},
-  { bgcolor: "#00695c", completed: 45 , title: "CSS"},
+  { bgcolor: "#6a1b9a", completed: 13 , title: "React"},
+  { bgcolor: "#231cca", completed: 8 , title: "NextJS"},
+  { bgcolor: "#00695c", completed: 30 , title: "CSS"},
   { bgcolor: "#ef6c00", completed: 45 , title: "HTML"},
-  { bgcolor: "#447799", completed: 47 , title: "JS"},
-  { bgcolor: "#128c24", completed: 45 , title: "Redux"},
-  { bgcolor: "#123499", completed: 44 , title: "C#"},
-  { bgcolor: "#993255", completed: 40 , title: "Unity"},
-  { bgcolor: "#5bca50", completed: 38 , title: "C++"},
+  { bgcolor: "#447799", completed: 20 , title: "JS"},
+  { bgcolor: "#128c24", completed: 15 , title: "Redux"},
+  { bgcolor: "#123499", completed: 20 , title: "C#"},
+  { bgcolor: "#993255", completed: 25 , title: "Unity"},
+  { bgcolor: "#5bca50", completed: 15 , title: "C++"},
   
 ];
 
